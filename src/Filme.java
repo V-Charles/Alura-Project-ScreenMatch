19cacf3cb0db4ff091ed45a90576773bfcc7e10c
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-01-26 18:33:31 -0300

    Enhance Filme class with evaluation methods and technical sheet display

--- src/Filme.java
+++ src/Filme.java
@@ -3,7 +3,20 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMediaDasAvaliacoes(){
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,12 +2,17 @@ public class Principal {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
+        meuFilme.nome = "O poderoço chefão";
         meuFilme.anoDeLancamento = 1970;
         meuFilme.duracaoEmMinutos = 180;
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancamento);
-        System.out.println(meuFilme.duracaoEmMinutos);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMediaDasAvaliacoes());
     }
 }
